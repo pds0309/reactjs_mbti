1279f78f315250abffd332fe356f9081dc72f473
pds0309 <ehd0309@gmail.com>
2022-04-26 00:03:45 +0900

    질문 테스트 기능 구현
    - 현재 몇 번 질문인지에 대한 상태와 상태 변경
    - 현재 스코어에 대한 상태와 상태 변경
    - 모든 질문에 대해 답변했을 때 결과 페이지로의 이동

--- catbti/src/pages/Question.js
+++ catbti/src/pages/Question.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import styled from "styled-components";
+
 import { ProgressBar, Button } from "react-bootstrap";
 import { QuestionData } from "../assets/data/QuestionData";
 
 const Question = () => {
+
+    const [questionNo, setQuestionNo] = useState(0);
+    const [totalScore, setTotalScore] = useState([
+            {id: "EI", score: 0},
+            {id: "SN", score: 0},
+            {id: "TF", score: 0},
+            {id: "JP", score: 0},
+    ]);
+    const navigate = useNavigate();
+    console.log("Current Progression :", questionNo);
+
+    const handleClickButton = (no, type) => {
+
+        const newScore = totalScore.map(s => s.id === type ? {id: s.id, score: s.score + no} : s);
+        
+        setTotalScore(newScore);
+        
+        if(QuestionData.length === questionNo + 1) {
+            navigate("/result");
+            // return;
+        }
+        setQuestionNo(questionNo + 1);
+    }
+
+    const handleClickButtonB = (no) => {
+        setQuestionNo(questionNo + 1);
+    }
+
     return (
         <Wrapper>
-            <ProgressBar striped variant="danger" now={80} />
-            <Title>{QuestionData[0].title}</Title>
+            <ProgressBar striped variant="danger" now={(questionNo / QuestionData.length) * 100} />
+            <Title>{QuestionData[questionNo].title}</Title>
             <ButtonGroup>
-                <Button style={{width:"40%", minHeight: "200px", fontSize: "15pt"}}>{QuestionData[0].answera}</Button>
-                <Button style={{width:"40%", minHeight: "200px", fontSize: "15pt", marginLeft: "20px"}}>{QuestionData[0].answerb}</Button>
+                <Button onClick={() => handleClickButton(1, QuestionData.type)} style={{ width: "40%", minHeight: "200px", fontSize: "15pt" }}>{QuestionData[questionNo].answera}</Button>
+                <Button onClick={() => handleClickButton(0, QuestionData.type)} style={{ width: "40%", minHeight: "200px", fontSize: "15pt", marginLeft: "20px" }}>{QuestionData[questionNo].answerb}</Button>
             </ButtonGroup>
         </Wrapper>
     )
